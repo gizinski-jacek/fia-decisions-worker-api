8efaacc6c1e516d6fdccfd19b86ab63211bb8adc
Jacek Gizinski <jacektrg+github@gmail.com>
2024-03-14 16:31:06 +0100

    Adding schema model for series data.

---
+++ models/seriesDataDoc.ts
@@ -0,0 +1,29 @@
+import mongoose from 'mongoose';
+import { SeriesDataDocModel } from '../types/myTypes';
+
+const Schema = mongoose.Schema;
+
+const Series_Data_Doc = new Schema<
+	Omit<SeriesDataDocModel, 'year'> & { year: Number }
+>(
+	{
+		series: { type: String, trim: true, required: true },
+		year: {
+			type: Number,
+			trim: true,
+			min: 2019,
+			max: new Date().getFullYear(),
+			required: true,
+		},
+		documents_url: {
+			type: String,
+			trim: true,
+			minlength: 64,
+			maxlength: 512,
+			required: true,
+		},
+	},
+	{ timestamps: true }
+);
+
+module.exports = Series_Data_Doc;
