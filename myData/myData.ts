7fb9b7169ec06ca83da7f8b50bdd4ae5892b0575
Jacek Gizinski <jacektrg+github@gmail.com>
2024-03-17 20:30:23 +0100

    Adding documents page of each series.

--- myData/myData.ts
+++ myData/myData.ts
@@ -4,6 +4,13 @@ export const fiaDomain: string = 'https://www.fia.com';
 // List of supported Formula series.
 export const supportedSeries: string[] = ['f1', 'f2', 'f3'];
 
+// Page with penalties documents for each series (no specific year).
+export const seriesDocumentsPage: { [key: string]: string } = {
+	f1: 'https://www.fia.com/documents/championships/championships/fia-formula-one-world-championship-14',
+	f2: 'https://www.fia.com/documents/championships/championships/formula-2-championship-44',
+	f3: 'https://www.fia.com/documents/championships/championships/fia-formula-3-championship-1012',
+};
+
 export const disallowedWordsInDocName: string[] = [
 	'reprimand',
 	'withdrawal',
